50376bfe9deea49328b69300a53f8e1877828457
kik_legion_note <kik87827@gmail.com>
2025-11-02 22:47:56 +0900

    스터디 진행중2

---
+++ index3.js
@@ -0,0 +1,8 @@
+var womanName = 'kim';
+var womanAge = 50;
+var womanMerry = true;
+var members = ['kim', 'park'];
+var members2 = { member1: 'kim', member2: 'kim' };
+var members3 = 'park';
+var data5 = 5;
+var data6 = [1, 2, 3];
