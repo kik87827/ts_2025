76954cde01e039161258b4ab05fa30d9534f5c91
kik_lenovo_17 <kik87827@gmail.com>
2025-11-07 18:56:19 +0900

    4강 완료

---
+++ homework2.js
@@ -0,0 +1,13 @@
+/* (숙제1) 다음 변수 4개에 타입을 지정해봅시다. */
+var user = 'kim';
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+/* (숙제2) 학교라는 변수에 타입지정해보십시오. */
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
---
+++ index4.ts
@@ -0,0 +1,15 @@
+let member2s :(number | string | boolean) = 123;
+
+member2s = 123;
+
+let member3s :(number | string)[] = [1,'2',3];
+
+let member4s :{ a: string | number} = {
+    a : '123'
+}
+
+member4s.a = 123;
+
+let age2 : string|number;
+
+let age3 :unknown = 1;
